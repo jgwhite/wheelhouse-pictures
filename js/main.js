700248d1d7cf9dbd890bd0acd37c74a11bf0a08c
Brendan Gatens <howdy@brendangatens.com>
2013-10-13 14:52:55 +0100

    Added left side background images

--- js/main.js
+++ js/main.js
@@ -12,4 +12,121 @@ $(document).ready(function() {
     return false;
     
   });
+  
+  $("#about-wheelhouse").waypoint(function(direction) {
+
+    if (direction == "down") {
+      $(".logo-flag-navy").fadeOut(1000);
+      $(".logo-flag-white").addClass("show");
+      $(".about-bg").addClass("show");
+      
+    } else {
+      $(".logo-flag-white").removeClass("show");
+      $(".logo-flag-navy").fadeIn(1000);
+      $(".about-bg").removeClass("show");
+    }
+    
+  }, { offset: 200});
+  
+  $("#mfk").waypoint(function(direction) {
+
+    if (direction == "down") {
+      $(".about-bg").removeClass("show");
+      $(".mfk-bg").addClass("show");
+      
+    } else {
+      $(".mfk-bg").removeClass("show");
+      $(".about-bg").addClass("show");
+    }
+    
+  }, { offset: 200});
+  
+  $("#awol").waypoint(function(direction) {
+
+    if (direction == "down") {
+      $(".mfk-bg").removeClass("show");
+      $(".awol-bg").addClass("show");
+      
+    } else {
+      $(".awol-bg").removeClass("show");
+      $(".mfk-bg").addClass("show");
+    }
+    
+  }, { offset: 200});
+  
+  $("#penny").waypoint(function(direction) {
+
+    if (direction == "down") {
+      $(".awol-bg").removeClass("show");
+      $(".penny-bg").addClass("show");
+      
+    } else {
+      $(".penny-bg").removeClass("show");
+      $(".awol-bg").addClass("show");
+    }
+    
+  }, { offset: 200});
+  
+  $("#stick").waypoint(function(direction) {
+
+    if (direction == "down") {
+      $(".penny-bg").removeClass("show");
+      $(".stick-bg").addClass("show");
+      
+    } else {
+      $(".stick-bg").removeClass("show");
+      $(".penny-bg").addClass("show");
+    }
+    
+  }, { offset: 200});
+  
+  
+  /*
+$("#about-wheelhouse").waypoint(function(direction) {
+
+    if (direction == "down") {
+      $(".logo").html("<img class=\"logo-flag\" src=\"images/wheelhouse-pictures-logo-white.svg\" alt=\"wheelhouse-pictures-flag\" onerror=\"this.onerror = null; this.src = 'images/wheelhouse-pictures-logo-white.png'\">");
+      $(".about-bg").addClass("show");
+      
+    } else {
+      $(".logo").html("<img class=\"logo-flag\" src=\"images/wheelhouse-pictures-logo-navy.svg\" alt=\"wheelhouse-pictures-flag\" onerror=\"this.onerror = null; this.src = 'images/wheelhouse-pictures-logo-navy.png'\">");
+      $(".about-bg").removeClass("show");
+    }
+    
+  }, { offset: 200});
+*/
+
+  
+  /*
+$("#current-slate section").waypoint(function(direction) {
+  
+    if (direction == "down") {
+      
+      // find the id of the particular section we just scrolled on
+      var id = $(this).attr("id");  
+      
+    } else {
+      
+      // find the id of the PREVIOUS section we scrolled on
+      var id = $(this).prev().attr("id");
+      
+    }
+    
+    var bg = $("." + id + "-bg");
+    
+    $(".logo-area").addClass("stick-bg");
+    console.log("." + id + "-bg");
+    
+    var nav = $("nav a[href=" + id + "]");
+    
+    nav.addClass("selected");
+    
+    nav.siblings().removeClass("selected");
+    
+  }, { offset: 200});
+*/
+  
+  
+  
+  
 });
